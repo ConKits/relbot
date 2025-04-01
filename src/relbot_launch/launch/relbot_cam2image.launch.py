f498ecb730e992cb45898f6a577f9d2f811e2d32
ConKits <costantinoskitsios@gmail.com>
2025-04-01 16:40:01 +0200

    Auto-commit: Tue Apr  1 04:40:01 PM CEST 2025

--- src/relbot_launch/launch/relbot_cam2image.launch.py
+++ src/relbot_launch/launch/relbot_cam2image.launch.py
@@ -4,13 +4,8 @@ from launch import LaunchDescription
 from launch_ros.actions import Node
 
 def generate_launch_description():
-    config_path = os.path.join(
-      get_package_share_directory('cam2image_vm2ros'),
-      'config',
-      'cam2image.yaml'
-      )
-
-    return LaunchDescription([
+   
+       return LaunchDescription([
         Node(
             package='cam2image_vm2ros',
             executable='cam2image',
@@ -20,16 +15,16 @@ def generate_launch_description():
             package="my_image_processor",
             executable="image_processor"
         ),
-        Node(
-            package="relbot_simulator",
-            executable="relbot_simulator",
-            name="relbot_simulator"
-            ),
-        Node(
-            package="relbot_sequence_controller",
-            executable="relbot_sequence_controller",
-            name="relbot_sequence_controller"
-        ),
+        # Node(
+        #     package="relbot_simulator",
+        #     executable="relbot_simulator",
+        #     name="relbot_simulator"
+        #     ),
+        # Node(
+        #     package="relbot_sequence_controller",
+        #     executable="relbot_sequence_controller",
+        #     name="relbot_sequence_controller"
+        # ),
         
     ])
 
